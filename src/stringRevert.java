ef1c7b1b129487bd094cb5b6e9a3155649fcd7cb
davitebralidze <dav.ebralidze@gmail.com>
2023-02-07 16:15:35 +0400

    Removing extra comments

--- src/stringRevert.java
+++ src/stringRevert.java
@@ -1,30 +1,3 @@
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Scanner;
-//
-//public class stringRevert {
-//
-//    public static void main(String[] args) {
-//
-//        Scanner scanner = new Scanner(System.in);
-//        String input;
-//
-//        System.out.println("Please enter a string");
-//        input = scanner.nextLine();
-//
-//        char[] dividedString = input.toCharArray();
-//        List<String> reversedList = new ArrayList<>();
-//        int maxIndex = dividedString.length - 1;
-//
-//        for (int i = maxIndex; i >= 0; i--) {
-//            reversedList.add(String.valueOf(dividedString[i]));
-//        }
-//
-//        System.out.println("Reversed version of your text is: " + String.join("", reversedList));
-//
-//    }
-//}
-
 import java.util.Scanner;
 
 public class stringRevert {
