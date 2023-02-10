c37845ec7c5d2818d58185ba0dced464d19ba554
davitebralidze <dav.ebralidze@gmail.com>
2023-02-10 12:14:34 +0400

    Saving last changes

--- src/scratchTest.java
+++ src/scratchTest.java
@@ -1,15 +1,49 @@
+import java.util.Scanner;
+
 public class scratchTest {
 
     public static void main(String[] args) {
+        //Write you scratch here:
 
-        double x = 3.3;
 
-        int y =(int)Math.round(x);
-        y*=y;
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Enter number of floors: ");
+        int nFloors = scr.nextInt();
 
-        System.out.println(y);
+        for (int l = 1; l <=nFloors; l++) {
+            preciseFloorBuilder(l, nFloors);
+        }
+
 
     }
+
+    public static void preciseFloorBuilder(int preciseFloor, int nFloors) {
+
+        int lastLine = 1 + (nFloors - 1) * 2;
+        int spaceMax = (lastLine - 1) / 2;
+        int preciseFloorSignAmount = 1 + (preciseFloor - 1) * 2;
+        int preciseFloorSpaceAmount = spaceMax + (preciseFloor - 1) * (-1);
+
+        for(int i=0; i<preciseFloorSpaceAmount; i++) {
+            System.out.print(" ");
+        }
+
+        for (int k = 0; k <preciseFloorSignAmount; k++) {
+            System.out.print("*");
+        }
+
+        for(int i=0; i<preciseFloorSpaceAmount; i++) {
+            System.out.print(" ");
+        }
+        System.out.println();
+
+    }
+
+
 }
+
+
+
+
 
 
