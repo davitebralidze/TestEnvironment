21fa31d222a8da5e705a84b697c7a78ce646dcc9
davitebralidze <dav.ebralidze@gmail.com>
2023-02-10 12:43:09 +0400

    Saving last changes

--- src/pineBuilder.java
+++ src/pineBuilder.java
@@ -3,37 +3,37 @@ import java.util.Scanner;
 public class pineBuilder {
 
     public static void main(String[] args) {
+        //Write you scratch here:
+
 
         Scanner scr = new Scanner(System.in);
         System.out.println("Enter number of floors: ");
         int nFloors = scr.nextInt();
 
-        for (int l = 1; l <= nFloors; l++) {
-            preciseFloorBuilder(l, nFloors);
-        }
-
+        preciseFloorBuilder(nFloors);
 
     }
 
-    public static void preciseFloorBuilder(int preciseFloor, int nFloors) {
+    public static void preciseFloorBuilder(int nFloors) {
 
-        int lastLine = 1 + (nFloors - 1) * 2;
-        int spaceMax = (lastLine - 1) / 2;
-        int preciseFloorSignAmount = 1 + (preciseFloor - 1) * 2;
-        int preciseFloorSpaceAmount = spaceMax + (preciseFloor - 1) * (-1);
+        for (int preciseFloor = 1; preciseFloor <= nFloors; preciseFloor ++) {
+            int lastLine = 1 + (nFloors - 1) * 2;
+            int spaceMax = (lastLine - 1) / 2;
+            int preciseFloorSignAmount = 1 + (preciseFloor - 1) * 2;
+            int preciseFloorSpaceAmount = spaceMax + (preciseFloor - 1) * (-1);
 
-        for (int i = 0; i < preciseFloorSpaceAmount; i++) {
-            System.out.print(" ");
+            for (int i = 0; i < preciseFloorSpaceAmount; i++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < preciseFloorSignAmount; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
         }
-
-        for (int k = 0; k < preciseFloorSignAmount; k++) {
-            System.out.print("*");
-        }
-
-        System.out.println();
-
     }
-
 
 }
 
