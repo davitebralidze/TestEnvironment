0761886cc570af91877f701be06f74d2876e0d4e
davitebralidze <dav.ebralidze@gmail.com>
2023-02-10 13:14:43 +0400

    Saving last changes

--- src/pyramidBuilder.java
+++ src/pyramidBuilder.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class pineBuilder {
+public class pyramidBuilder {
 
     public static void main(String[] args) {
 
@@ -10,15 +10,21 @@ public class pineBuilder {
         while (!scr.hasNextInt()) {
             System.out.println("The height of the tree must be an integer. Please enter again: ");
             scr.next();
-        } int nFloors = scr.nextInt();
+        }
+        int nFloors = scr.nextInt();
+
+        System.out.println();
 
         preciseFloorBuilder(nFloors);
+
+
 
     }
 
     public static void preciseFloorBuilder(int nFloors) {
 
-        for (int preciseFloor = 1; preciseFloor <= nFloors; preciseFloor ++) {
+
+        for (int preciseFloor = 1; preciseFloor <= nFloors; preciseFloor++) {
 
             int lastLine = 1 + (nFloors - 1) * 2;
             int spaceMax = (lastLine - 1) / 2;
@@ -36,6 +42,7 @@ public class pineBuilder {
             System.out.println();
 
         }
+
     }
 
 }
