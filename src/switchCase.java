5fb2bb9b4421e03d7b7a3e8e39160a68c88014d3
davitebralidze <dav.ebralidze@gmail.com>
2023-05-01 16:39:50 +0400

    Saving last changes

---
+++ src/switchCase.java
@@ -0,0 +1,27 @@
+public class switchCase {
+    public static void main(String[] args) {
+
+        System.out.println(monthQuarter("JULY"));
+
+    }
+
+    public static String monthQuarter (String month) {
+
+        return switch (month) {
+            case "JANUARY" , "FEBRUARY" , "MARCH" -> "1st quarter";
+//            case "JANUARY" , "FEBRUARY" , "MARCH" -> {yield "1st quarter";}
+//            yield is needed if we are using curly braces
+            case "APRIL" , "MAY" , "JUNE" -> "2nd quarter";
+            case "JULY" , "AUGUST" , "SEPTEMBER" -> "3rd quarter";
+            case "OCTOBER" , "NOVEMBER" , "DECEMBER" -> "4th quarter";
+//          default -> month + " is not a month";
+            default -> {
+                String badResponse = month + " is not a month";
+                yield badResponse;
+            }
+        };
+
+
+    }
+
+}
---
+++ src/ternaryOperator.java
@@ -0,0 +1,21 @@
+public class ternaryOperator {
+    public static void main(String[] args) {
+
+        int num1 = 25;
+        int num2 = 20;
+        int result;
+
+
+        result = (num1 > num2) ? num1 : num2;
+
+        System.out.println(result);
+
+
+    }
+
+}
+
+
+
+
+
