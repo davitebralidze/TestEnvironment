71c4062f6064c9e453b63511f88dc0beb138e7c7
davitebralidze <dav.ebralidze@gmail.com>
2023-07-12 17:54:30 +0400

    Saving last changes

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -22,15 +22,12 @@ public class PasswordGenerator {
 
     public static String passwordGenerator(int passwordLength) {
         if (passwordLength < 4 || passwordLength > 30) {
-                throw new IllegalArgumentException();
-            }
+            throw new IllegalArgumentException();
+        }
 
         Random random = new Random();
         StringBuilder generatedPassword = new StringBuilder();
-        char[] charactersToBeUsed = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0',
-                '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'};
+        char[] charactersToBeUsed = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'};
         int randIndex = charactersToBeUsed.length - 1;
         for (int i = 0; i < passwordLength; i++) {
             generatedPassword.append(charactersToBeUsed[random.nextInt(randIndex)]);
